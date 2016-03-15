0c7bf9f60ee4ed28ee90a2a3199fd4cc9028cde8
SupunS <supuns@wso2.com>
2016-03-15 17:17:19 +0530

    Fixing null. Adding Mediator messgaes table

--- features/org.wso2.carbon.analytics.esb.feature/src/main/capp/GadgetMessageTable_1.0.0/Message_Table/js/gadgetconf.js
+++ features/org.wso2.carbon.analytics.esb.feature/src/main/capp/GadgetMessageTable_1.0.0/Message_Table/js/gadgetconf.js
@@ -16,5 +16,14 @@ var configs = [{
             { name: "startTime", label: "Timestamp", type: "ordinal" },
             { name: "status", label: "Message Status", type: "ordinal" }
         ]
+    },{
+        name: TYPE_MEDIATOR,
+        type: 19,
+        columns: [
+            { name: "messageFlowId", label: "Message ID", type: "ordinal" },
+            { name: "host", label: "Host", type: "ordinal" },
+            { name: "startTime", label: "Timestamp", type: "ordinal" },
+            { name: "status", label: "Message Status", type: "ordinal" }
+        ]
     }
 ];
