9fdcf70d28797af55ee9164af44382b32a077405
SupunS <supuns@wso2.com>
2016-03-16 16:28:42 +0530

    Adding Srquence search and message flow

--- features/org.wso2.carbon.analytics.esb.feature/src/main/capp/GadgetMessageTable_1.0.0/Message_Table/js/gadgetconf.js
+++ features/org.wso2.carbon.analytics.esb.feature/src/main/capp/GadgetMessageTable_1.0.0/Message_Table/js/gadgetconf.js
@@ -34,5 +34,14 @@ var configs = [{
             { name: "startTime", label: "Timestamp", type: "ordinal" },
             { name: "status", label: "Message Status", type: "ordinal" }
         ]
+    },{
+        name: TYPE_SEQUENCE,
+        type: 31,
+        columns: [
+            { name: "messageFlowId", label: "Message ID", type: "ordinal" },
+            { name: "host", label: "Host", type: "ordinal" },
+            { name: "startTime", label: "Timestamp", type: "ordinal" },
+            { name: "status", label: "Message Status", type: "ordinal" }
+        ]
     }
 ];
--- features/org.wso2.carbon.analytics.esb.feature/src/main/capp/GadgetSearchBox_1.0.0/Search_Box/js/gadgetconf.js
+++ features/org.wso2.carbon.analytics.esb.feature/src/main/capp/GadgetSearchBox_1.0.0/Search_Box/js/gadgetconf.js
@@ -2,5 +2,6 @@ var configs = [
     { name: TYPE_PROXY, type: 6 },
     { name: TYPE_API, type: 11 },
     { name: TYPE_MEDIATOR, type: 16 },
-    { name: TYPE_ENDPOINT, type: 24 }
+    { name: TYPE_ENDPOINT, type: 24 },
+    { name: TYPE_SEQUENCE, type: 28 }
 ];
