ba1ee12bae6282f1135f24be5f888c87cd7074dd
SupunS <supuns@wso2.com>
2016-03-17 13:42:29 +0530

    Adding message flow for Sequences and Inbound Endpoints

--- features/org.wso2.carbon.analytics.esb.feature/src/main/capp/GadgetMessageFlow_1.0.0/Message_Flow/js/gadgetconf.js
+++ features/org.wso2.carbon.analytics.esb.feature/src/main/capp/GadgetMessageFlow_1.0.0/Message_Flow/js/gadgetconf.js
@@ -3,5 +3,5 @@ var configs = [
     { name: TYPE_API, type: 10},
     { name: TYPE_MESSAGE, type: 22},
     { name: TYPE_SEQUENCE, type: 32},
-    { name: TYPE_INBOUND_ENDPOINT, type: 37}
+    { name: TYPE_INBOUND_ENDPOINT, type: 10}
 ];
