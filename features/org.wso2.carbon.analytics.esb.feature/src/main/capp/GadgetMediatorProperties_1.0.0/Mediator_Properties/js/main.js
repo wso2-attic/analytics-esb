a8ad65d8d257d0652f7db639280fdc63b00c1422
Dunith Dhanushka <dunithd@gmail.com>
2016-03-25 12:05:18 +0530

    Adding Mediator_Properties gadget and wired with the backend

---
+++ features/org.wso2.carbon.analytics.esb.feature/src/main/capp/GadgetMediatorProperties_1.0.0/Mediator_Properties/js/main.js
@@ -0,0 +1,82 @@
+var TOPIC = "subscriber";
+var type = 45;
+var qs = gadgetUtil.getQueryString();
+var BEFORE = "before";
+var AFTER = "after";
+
+$(function() {
+    if (qs[PARAM_ID] == null) {
+        $("#gadget-message").html(gadgetUtil.getDefaultText());
+        $("#gadget-message").show();
+        $("#props").hide();
+        return;
+    } else {
+        $("#gadget-message").hide();
+        $("#props").show();
+    }
+});
+
+gadgets.HubSettings.onConnect = function() {
+    gadgets.Hub.subscribe(TOPIC, function(topic, data, subscriberData) {
+        mediatorClicked(data);
+    });
+};
+
+function mediatorClicked(data) {
+    var mediatorId = data.mediatorId;
+    if(mediatorId) {
+        gadgetUtil.fetchData(CONTEXT, {
+            type: type,
+            id: qs.id,
+            mediatorId: mediatorId
+        }, onData, onError);
+    }
+};
+
+function onData(response) {
+    try {
+        var data = response.message;
+        console.log(data); 
+        if (!data) {
+            $("#canvas").html(gadgetUtil.getEmptyRecordsText());
+            return;
+        }
+        if(data.payload.before) {
+            $("#payloadBfr").text(data.payload.before);
+        }
+        if(data.payload.after) {
+            $("#payloadAftr").text(data.payload.after);
+        }
+
+        if(data.transportProperties) {
+           drawPropertyTable(data.transportProperties,$("#tblTransportBfr tbody"),BEFORE);
+           drawPropertyTable(data.transportProperties,$("#tblTransportAftr tbody"),AFTER);
+        }
+        if(data.contextProperties) {
+           drawPropertyTable(data.contextProperties,$("#tblCtxtBfr tbody"),BEFORE);
+           drawPropertyTable(data.contextProperties,$("#tblCtxtAftr tbody"),AFTER);
+        }
+    } catch (e) {
+        $("#gadget-message").html(gadgetUtil.getErrorText(e));
+    }
+};
+
+function onError(msg) {
+    $("#gadget-message").html(gadgetUtil.getErrorText(msg));
+};
+
+function drawPropertyTable(properties,tbody,side) {
+    tbody.empty();
+    properties.forEach(function (property) {
+        var tr = jQuery('<tr/>');
+        var tdKey = jQuery('<td/>');
+        var tdValue = jQuery('<td/>');
+        tr.append(tdKey.append(property.name));
+        if(side === BEFORE) {
+            tr.append(tdValue.append(property.before));
+        } else {
+            tr.append(tdValue.append(property.after));
+        }
+        tr.appendTo(tbody);
+    });
+}
