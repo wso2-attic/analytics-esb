86ce4c05c781acb805ff664abf01827f21594087
Jerad Rutnam <jeradrutnam@outlook.com>
2016-07-07 20:28:02 +0530

    Updated autocomplete component with new DS controller feature

--- features/org.wso2.analytics.esb.feature/src/main/capp/dashboard/GadgetSearchBox_1.0.0/Search_Box/js/main.js
+++ features/org.wso2.analytics.esb.feature/src/main/capp/dashboard/GadgetSearchBox_1.0.0/Search_Box/js/main.js
@@ -23,20 +23,9 @@ $(function() {
             hint: true,
             highlight: true,
             minLength: 0
-        }, {
+        },{
             name: 'proxyName',
             source: substringMatcher(response.message)
-        }).on('typeahead:rendered', function() {
-            var typeAhead = $('.tt-menu'),
-                parentWindow = window.parent.document,
-                thisParentWrapper = $('#' + gadgets.rpc.RPC_ID, parentWindow).closest('.grid-stack-item');
-
-            $('head', parentWindow).append('<link rel="stylesheet" type="text/css" href="'+resolveURI+'store/carbon.super/gadget/Search_Box/css/autocomplete.css" />');
-            $('body', parentWindow).append('<script src="'+resolveURI+'store/carbon.super/gadget/Search_Box/js/typeahead.bundle.js" type="text/javascript"></script>');
-            //console.log(typeAhead);
-            $(thisParentWrapper).append(typeAhead);
-            $(thisParentWrapper).closest('.ues-component-box').addClass('widget form-control-widget');
-            $('body').addClass('widget');
         }).on('typeahead:selected', function(evt, item) {
             var href = parent.window.location.href;
             if(qs[PARAM_ID]) {
@@ -49,6 +38,12 @@ $(function() {
             }
             // console.log(href); 
             parent.window.location = href;
+        }).on('typeahead:open', function(evt, item) {
+            wso2.gadgets.controls.resizeGadget({
+                height: "200px"
+            }); 
+        }).on('typeahead:close', function(evt, item) {
+            wso2.gadgets.controls.restoreGadget();
         }).focus().blur();
     }
 
